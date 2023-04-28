75bcf15c6ac356f9b49fcc6fd1a49c7733559bbc
Umesh U <umeshtechdeveloper@gmail.com>
2023-04-28 13:11:43 +0530

    encrypted password using mongoose-encryption module

--- models/user.js
+++ models/user.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 
-const userSchema = {
+const userSchema =  new mongoose.Schema({
     email: String,
     password: String 
-};
+});
 
+userSchema.plugin(encrypt, { secret : process.env.MONGO_SECRET, encryptedFields: ["password"] });
 module.exports = new mongoose.model('User', userSchema);
