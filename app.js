21947203c59234f524e18adaeff5317344b0a54c
Umesh U <umeshtechdeveloper@gmail.com>
2023-04-28 13:27:38 +0530

    password is hashed using md5 module

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const md5 = require('md5');
 
 const port = process.env.PORT || 3000;
 const User = require('./models/user');
@@ -35,7 +36,7 @@ app.get('/register', function(req, res){
 app.post('/register', function(req, res){
     const newUser= new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save()
@@ -45,7 +46,7 @@ app.post('/register', function(req, res){
 
 app.post('/login', function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username})
         .then( (foundUser) => {
--- models/user.js
+++ models/user.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
 
 const userSchema =  new mongoose.Schema({
     email: String,
     password: String 
 });
 
-userSchema.plugin(encrypt, { secret : process.env.MONGO_SECRET, encryptedFields: ["password"] });
 module.exports = new mongoose.model('User', userSchema);
